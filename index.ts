521dff91726cdc6fb86490adc674bf5eca7c0847
Veysel13 <veyselakpinar13@gmail.com>
2025-04-24 08:28:31 +0300

    changed name

--- index.ts
+++ index.ts
@@ -7,6 +7,7 @@ if (!process.env.JWT_KEY) {
 const PORT = process.env.PORT || 3000;
 const ENV  = process.env.ENVIRONMENT || 'local';
 
+
 if(ENV == 'production')
 server.listen(PORT, '0.0.0.0', () => console.log(`Server is live at localhost:${PORT}`));
 else
